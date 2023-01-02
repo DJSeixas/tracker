b5ed413589bfb135f9bd05b371a2e00faf1ae31c
DaniloSeixas <dnl.seixas@hotmail.com>
2023-01-02 16:03:36 -0300

    v7

--- src/services/services.js
+++ src/services/services.js
@@ -10,6 +10,6 @@ const apiClient = axios.create({
 // eslint-disable-next-line
 export default{
     getInfoByIp(info){
-        return apiClient.get(`country,city?apiKey=dy64ZC4u6AHKsrmwNAMgAaz512y1x&domain=${info}`)
+        return apiClient.get(`country,city?apiKey=at_OfNEVZsl7fLS0EkI5b0mDl9TwdVhm&domain=${info}`)
     }
 }
